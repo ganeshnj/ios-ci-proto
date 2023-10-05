af2faf8e914515e8ec7bdfd12379743708701b48
Ganesh Jangir <ganesh.jangir@datadoghq.com>
2023-10-05 17:30:15 +0200

    bootstrap

---
+++ my-app/my-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  my-app
+//
+//  Created by Ganesh Jangir on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ my-app/my-app/my_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_appApp.swift
+//  my-app
+//
+//  Created by Ganesh Jangir on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct my_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
